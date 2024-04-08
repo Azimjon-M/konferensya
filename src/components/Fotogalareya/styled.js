0bef5929e6f88d9b26946b63d0eb661ed6fe62e0
Orifxon3595 <orifxontuychiyev@gmail.com>
2024-04-08 09:31:29 +0500

    update

--- src/components/Fotogalareya/styled.js
+++ src/components/Fotogalareya/styled.js
@@ -1,9 +1,9 @@
 import styled, { keyframes } from "styled-components";
 
 export const Marquee = styled.div`
-    display: flex;
-    overflow: hidden;
-    user-select: none;
+  display: flex;
+  overflow: hidden;
+  user-select: none;
 `;
 
 export const scrollX = keyframes`
@@ -15,26 +15,25 @@ transform: translateX(-100%);
 }`;
 
 export const MarqueeGroup = styled.div`
-    flex-shrink: 0;
-    display: flex;
-    align-items: center;
-    justify-content: space-around;
-    white-space: nowrap;
-    animation: ${scrollX} 40s linear infinite;
-    
+  flex-shrink: 0;
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  white-space: nowrap;
+  animation: ${scrollX} 40s linear infinite;
 `;
 export const ImageGroup = styled.div`
-    /* width: 400px;
+  /* width: 400px;
     height: 250px; */
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    margin: 0 5px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 0 5px;
 `;
 
 export const Image = styled.img`
-    object-fit: cover;
-    width: 100%;
-    height: auto;
-    aspect-ratio: 16/16;
+  object-fit: cover;
+  width: 100%;
+  height: auto;
+  aspect-ratio: 16/16;
 `;
