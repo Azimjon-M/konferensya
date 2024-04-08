01e9594dc4a528952004a16018e00d605ea9d68b
Azimjon <azimeliboev2000@gmail.com>
2024-04-08 10:27:04 +0500

    Home Welcome ++

--- src/components/WelcomeSec/index.jsx
+++ src/components/WelcomeSec/index.jsx
@@ -7,7 +7,7 @@ const WelcomeSec = () => {
     const [data, setData] = useState(null);
 
     const getData = async () => {
-        await APIHome.getCarousel()
+        await APIHome.getWelcome()
             .then((res) => setData(res.data))
             .catch((err) => console.log(err));
     };
@@ -17,32 +17,24 @@ const WelcomeSec = () => {
     console.log(data);
 
     return (
-        <div className="flex flex-col bg-gray-50">
+        <>
             {data &&
                 data.map((item) => (
-                    <>
+                    <div key={item.id} className="flex flex-col bg-gray-50">
                         <div className="bg-gray-200 px-4 py-4 sm:px-8 md:px-12 md:py-8 lg:px-16">
                             <h1 className="font-medium sm:text-[18px] lg:text-[20px] xl:text-[22px] 2xl:text-[24px]">
                                 {item && item[`title_${Lang}`]}
                             </h1>
                         </div>
                         <div className="p-5 sm:p-8 md:px-12 lg:px-16 lg:text-[17px] xl:text-[18px] 2xl:text-[20px]">
-                            <p className="">
-                                {item& item[`body_1_${Lang}`]}
-                            </p>
-                            <p className="">
-                                {item& item[`body_2_${Lang}`]}
-                            </p>
-                            <p className="">
-                                {item& item[`body_3_${Lang}`]}
-                            </p>
-                            <p className="">
-                                {item& item[`body_4_${Lang}`]}
-                            </p>
+                            <p className="">{item && item[`body_1_${Lang}`]}</p>
+                            <p className="">{item && item[`body_2_${Lang}`]}</p>
+                            <p className="">{item && item[`body_3_${Lang}`]}</p>
+                            <p className="">{item && item[`body_4_${Lang}`]}</p>
                         </div>
-                    </>
+                    </div>
                 ))}
-        </div>
+        </>
     );
 };
 
