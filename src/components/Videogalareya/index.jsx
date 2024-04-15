93cf05771600d4b83109fbad761657655dd4498d
Azimjon <azimeliboev2000@gmail.com>
2024-04-15 10:16:31 +0500

    Home API fetch data ++

--- src/components/Videogalareya/index.jsx
+++ src/components/Videogalareya/index.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/pagination";
@@ -6,49 +6,60 @@ import "../App/style.css";
 import { Pagination, Navigation } from "swiper/modules";
 import video from "../../assets/video/train.mp4";
 import TranslateMessage from "../TranslateMessage";
+import APIHome from "../../services/home";
 
 const Videogalareya = () => {
+    const [data, setData] = useState(null);
+
+    const getData = async () => {
+        await APIHome.getVideoGalareya()
+            .then((res) => setData(res.data))
+            .catch((err) => console.log(err));
+    };
+    useEffect(() => {
+        getData();
+    }, []);
     return (
         <div className="flex flex-col bg-gray-100">
             <div className="bg-gray-200 px-4 py-4 sm:px-8 md:px-12 md:py-8 lg:px-16">
                 <h1 className="font-medium sm:text-[18px] lg:text-[20px] xl:text-[22px] 2xl:text-[24px]">
-                    <TranslateMessage id="Videogalareya" /> 
+                    <TranslateMessage id="Videogalareya" />
                 </h1>
             </div>
             <div className="w-full bg-gray-50 py-10">
-                <Swiper
-                    slidesPerView={1}
-                    spaceBetween={10}
-                    navigation={true}
-                    loop={true}
-                    pagination={{
-                        clickable: true,
-                        type: "progressbar",
-                    }}
-                    breakpoints={{
-                        640: {
-                            slidesPerView: 2,
-                        },
-                        768: {
-                            slidesPerView: 3,
-                        },
-                    }}
-                    modules={[Pagination, Navigation]}
-                    className="mySwiper"
-                >
-                    <SwiperSlide>
-                        <video className="-z-20" controls src={video}></video>
-                    </SwiperSlide>
-                    <SwiperSlide>
-                        <video className="-z-20" controls src={video}></video>
-                    </SwiperSlide>
-                    <SwiperSlide>
-                        <video className="-z-20" controls src={video}></video>
-                    </SwiperSlide>
-                    <SwiperSlide>
-                        <video className="-z-20" controls src={video}></video>
-                    </SwiperSlide>
-                </Swiper>
+                {data && (
+                    <Swiper
+                        slidesPerView={1}
+                        spaceBetween={10}
+                        navigation={true}
+                        loop={true}
+                        pagination={{
+                            clickable: true,
+                            type: "progressbar",
+                        }}
+                        breakpoints={{
+                            640: {
+                                slidesPerView: 2,
+                            },
+                            768: {
+                                slidesPerView: 3,
+                            },
+                        }}
+                        modules={[Pagination, Navigation]}
+                        className="mySwiper"
+                    >
+                        {data &&
+                            data.map((item) => (
+                                <SwiperSlide key={item.id}>
+                                    <video
+                                        className="-z-20"
+                                        controls
+                                        src={item.video}
+                                    ></video>
+                                </SwiperSlide>
+                            ))}
+                    </Swiper>
+                )}
             </div>
         </div>
     );
