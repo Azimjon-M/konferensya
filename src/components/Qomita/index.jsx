7dbea7e52ade47bdb6c761dcbffbe609ba5e3d0f
Orifxon3595 <orifxontuychiyev@gmail.com>
2024-04-15 11:19:34 +0500

    update

--- src/components/Qomita/index.jsx
+++ src/components/Qomita/index.jsx
@@ -2,13 +2,41 @@ import React, { useEffect, useState } from "react";
 import APIQomita from "../../services/qomita";
 
 function Qomita() {
-  const [qomita, setQomita] = useState([]);
+  const [hammuallif, setHammuallif] = useState([]);
+  const [homiy, setHomiy] = useState([]);
+  const [ilmiydastur, setIlmiydastur] = useState([]);
+  const [kotibiyat, setKotibiyat] = useState([]);
+  const [logistika, setLogistika] = useState([]);
+  const [maslahatchi, setMaslahatchi] = useState([]);
+  const [rais, setRais] = useState([]);
+  const [raisorinbosari, setRaisorinbosari] = useState([]);
+  const [reklama, setReklama] = useState([]);
+  const [xazinachi, setXazinachi] = useState([]);
 
   useEffect(() => {
     const fechtQomita = async () => {
       try {
-        const response = await APIQomita.hammuallif();
-        setQomita(response.data);
+        const resHammuallif = await APIQomita.hammuallif();
+        const resHomiy = await APIQomita.homiy();
+        const resIlmiydastur = await APIQomita.ilmiydastur();
+        const resKotibiyat = await APIQomita.kotibiyat();
+        const resLogistika = await APIQomita.logistika();
+        const resMaslahatchi = await APIQomita.maslahatchi();
+        const resRais = await APIQomita.rais();
+        const resRaisorinbosari = await APIQomita.raisorinbosari();
+        const resReklama = await APIQomita.reklama();
+        const resXazinachi = await APIQomita.xazinachi();
+        
+        setHammuallif(resHammuallif.data);
+        setHomiy(resHomiy.data);
+        setIlmiydastur(resIlmiydastur.data);
+        setKotibiyat(resKotibiyat.data);
+        setLogistika(resLogistika.data);
+        setMaslahatchi(resMaslahatchi.data);
+        setRais(resRais.data);
+        setRaisorinbosari(resRaisorinbosari.data);
+        setReklama(resReklama.data);
+        setXazinachi(resXazinachi.data);
       } catch (error) {
         console.error("Ma'lumotlarni olishda xatolik yuz berdi:", error);
       }
@@ -16,7 +44,7 @@ function Qomita() {
     fechtQomita();
   }, []);
 
-  console.log(qomita);
+  console.log(hammuallif, homiy, ilmiydastur, maslahatchi, rais);
 
   return (
     <div>
