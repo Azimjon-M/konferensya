18e5db4eb09d680dca05978e487ba8f78db6c728
Orifxon3595 <orifxontuychiyev@gmail.com>
2024-04-17 16:25:28 +0500

    update

--- src/components/Nashr/index.jsx
+++ src/components/Nashr/index.jsx
@@ -1,7 +1,11 @@
 import React, { useEffect, useState } from "react";
 import APINash from "../../services/nashr";
+import TranslateMessage from "../TranslateMessage";
+import { useSelector } from "react-redux";
 
 function Nashr() {
+
+  const Lang = useSelector((state) => state.reducerLang.Lang)
   const [data, setData] = useState([]);
 
   useEffect(() => {
@@ -19,7 +23,9 @@ function Nashr() {
   return (
     <div>
       <div className="bg-gray-200  text-center py-6 lg:py-10 xl:py-16 2xl:py-20 ">
-        <h1 className="text-[2em] lg:text-[4em] font-semibold">Nashr</h1>
+        <h1 className="text-[2em] lg:text-[4em] font-semibold">
+          <TranslateMessage id="navDropNashr" />
+        </h1>
       </div>
       {data &&
         data.map((item) => {
@@ -35,15 +41,15 @@ function Nashr() {
                 </div>
                 <div className="col-span-2 border md:border-l-0 px-5 py-4 flex flex-col justify-center">
                   <a href="sss" className="text-2xl text-blue-700">
-                    {item.title_uz}
+                    {item[`title_${Lang}`]}
                   </a>
                   <h3 className="text-2xl text-gray-600 font-bold">
-                    {item.subtitle_uz}
+                  {item[`subtitle_${Lang}`]}
                   </h3>
                 </div>
               </div>
               <div className="mt-8">
-                <p className="text-lg md:text-2xl">{item.content_uz}</p>
+                <p className="text-lg md:text-2xl">{item[`content_${Lang}`]}</p>
               </div>
             </div>
           );
