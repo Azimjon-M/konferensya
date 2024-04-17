22a53d77869952d74bdccdbb7d23b621f272e35f
Orifxon3595 <orifxontuychiyev@gmail.com>
2024-04-17 10:30:18 +0500

    update

--- src/components/IMQomita/index.jsx
+++ src/components/IMQomita/index.jsx
@@ -17,17 +17,17 @@ function IMQomita() {
   }, []);
   return (
     <div>
-      <div class="max-w-6xl mx-auto my-20 relative overflow-x-auto shadow-md sm:rounded-lg">
-        <table class="w-full text-lg text-left rtl:text-right text-gray-500 dark:text-gray-400">
-          <thead class="text-lg text-gray-700 uppercase bg-blue-100 dark:bg-gray-700 dark:text-gray-400">
+      <div className="max-w-6xl mx-auto my-20 relative overflow-x-auto shadow-md sm:rounded-lg">
+        <table className="w-full text-lg text-left rtl:text-right text-gray-500 dark:text-gray-400">
+          <thead className="text-lg text-gray-700 uppercase bg-blue-100 dark:bg-gray-700 dark:text-gray-400">
             <tr>
-              <th scope="col" class=" w-[5%] px-6 py-3">
+              <th scope="col" className=" w-[5%] px-6 py-3">
                 №
               </th>
-              <th scope="col" class="px-6 py-3">
+              <th scope="col" className="px-6 py-3">
                 Ism
               </th>
-              <th scope="col" class="px-6 py-3">
+              <th scope="col" className="px-6 py-3">
                 Mansublik
               </th>
             </tr>
@@ -36,15 +36,15 @@ function IMQomita() {
             {data &&
               data.map((item, idx) => {
                 return (
-                  <tr key={idx} class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                  <tr key={idx} className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
                     <th
                       scope="row"
-                      class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                      className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
                     >
                       {idx}.
                     </th>
-                    <td class="px-6 py-4">{item.name_uz}</td>
-                    <td class="px-6 py-4">{item.oqish_joyi_uz}</td>
+                    <td className="px-6 py-4">{item.name_uz}</td>
+                    <td className="px-6 py-4">{item.oqish_joyi_uz}</td>
                   </tr>
                 );
               })}
