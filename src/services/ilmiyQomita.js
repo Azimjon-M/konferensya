1ae040c183618ae8c75e9010b0acbd76672cd7e3
Nodirbek Nurmamatov <103112982+nodirbek27@users.noreply.github.com>
2024-08-16 12:13:43 +0500

    ilmiyQomita++

--- src/services/ilmiyQomita.js
+++ src/services/ilmiyQomita.js
@@ -1,9 +1,25 @@
 import axiosInstance from ".";
 
-const endPoint = "talab/ilmiyqomita/";
+const ep = "talab/ilmiyqomita/";
+const epPost = "talab/ilmiyqomita/create/";
+const epPut = "talab/ilmiyqomita/update/";
+const epDel = "talab/ilmiyqomita/delete/";
 
-const getIlmiyQomita = () => axiosInstance.get(endPoint);
+const getIlmiyQomita = () => axiosInstance.get(ep);
 
-const APIIlmiyQomita = { getIlmiyQomita };
+const getbyId = (id) => {
+    return axiosInstance.get(`${ep}${id}/`);
+  };
+  const post = (item) => {
+    return axiosInstance.post(`${epPost}`, item);
+  };
+  const put = (id, item) => {
+    return axiosInstance.put(`${epPut}${id}/`, item);
+  };
+  const del = (id) => {
+    return axiosInstance.delete(`${epDel}${id}/`);
+  };
+
+const APIIlmiyQomita = { getIlmiyQomita, getbyId, post, put, del };
 
 export default APIIlmiyQomita;
